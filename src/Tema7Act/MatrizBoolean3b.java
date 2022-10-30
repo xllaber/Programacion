a67aefee02f3587014e7f03b8c1c4c53200269d8
Ximo Llacer <xllaber7@gmail.com>
2022-10-30 12:12:09 +0100

    Fixed some variables changes

--- src/Tema7Act/MatrizBoolean3b.java
+++ src/Tema7Act/MatrizBoolean3b.java
@@ -4,7 +4,7 @@ public class MatrizBoolean3b {
     public static void main(String[] args) {
         boolean[][] matrix0 = {{true, true, true, true, true}, {true, false, false, false, true}, {true, false, false, false, true}, {true, false, false, false, true}, {true, false, false, false, true}, {true, false, false, false, true}, {true, false, false, false, true}, {true, true, true, true, true}};
 
-        boolean[][] matrix2 = {{false, false, false, false, true}, {false, false, false, true, true}, {false, false,  true, false, true}, {false, true, false, false, true}, {true, false, false, false, true}, {false, false, false, false, true},{false, false, false, false, true}, {false, false, false, false, true}};
+        boolean[][] matrix1 = {{false, false, false, false, true}, {false, false, false, true, true}, {false, false,  true, false, true}, {false, true, false, false, true}, {true, false, false, false, true}, {false, false, false, false, true},{false, false, false, false, true}, {false, false, false, false, true}};
 
         for (boolean[] elementRow : matrix0) {
             for (boolean elementCol : elementRow) {
@@ -17,7 +17,7 @@ public class MatrizBoolean3b {
             System.out.println();
         }
         System.out.println();
-        for (boolean[] elementRow : matrix2) {
+        for (boolean[] elementRow : matrix1) {
             for (boolean elementCol : elementRow) {
                 if (elementCol) {
                    System.out.print("0");
