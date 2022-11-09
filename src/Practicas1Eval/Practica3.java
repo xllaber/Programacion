7f046adfb1341475ceaffe2a1fb0932dc8ceaf3d
Ximo Llacer <xllaber7@gmail.com>
2022-11-09 10:24:50 +0100

    made generateBow function

--- src/Practicas1Eval/Practica3.java
+++ src/Practicas1Eval/Practica3.java
@@ -10,8 +10,36 @@ public class Practica3 {
     public static Integer[][] board = new Integer[boardSize][boardSize];
 
     public static void main(String[] args) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][i] = 0;
+            }
+        }
+        generateBoat();
+        showBoard();
+    }
+
+    public static void generateBoat(){
         Random boatRow = new Random();
         Random boatCol = new Random();
+        for (int i = 0; i <= 6; i++) {
+            int row = boatRow.nextInt(9);
+            int col = boatCol.nextInt(9);
+            board[row][col] = 3;
+        }
+    }
+
+    public static void showBoard(){
+        for (Integer[] row : board) {
+            for (Integer col : row) {
+                if (col == 0){
+                    System.out.print("( )");
+                }else{
+                    System.out.print(" 0 ");
+                }
+            }
+            System.out.println();
+        }
     }
 
 
