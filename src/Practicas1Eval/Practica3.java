6570c9310198e9b130c7bf6bb1f1ae660036dbbd
Ximo Llacer <xllaber7@gmail.com>
2022-11-09 09:43:19 +0100

    Started programming P3

--- src/Practicas1Eval/Practica3.java
+++ src/Practicas1Eval/Practica3.java
@@ -1,5 +1,18 @@
 package Practicas1Eval;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Practica3 {
-    
+
+    public static final int numBoats = 6;
+    public static final int boardSize = 8;
+    public static Integer[][] board = new Integer[boardSize][boardSize];
+
+    public static void main(String[] args) {
+        Random boatRow = new Random();
+        Random boatCol = new Random();
+    }
+
+
 }
