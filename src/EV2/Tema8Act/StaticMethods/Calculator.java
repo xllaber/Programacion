f7c389e3e58f318e0e45f38ab3fe638a1287e042
Ximo Llacer <xllaber7@gmail.com>
2022-12-20 09:24:10 +0100

    Made calculator exercices

---
+++ src/EV2/Tema8Act/StaticMethods/Calculator.java
@@ -0,0 +1,33 @@
+package EV2.Tema8Act.StaticMethods;
+
+public class Calculator {
+
+    private static int result;
+
+    public static int add(int num1, int num2){
+        result = num1 + num2;
+        return result;
+    }
+
+    public static int substract(int num1, int num2){
+        result = num1 - num2;
+        return result;
+    }
+
+    public static int multiply(int num1, int num2){
+        result = num1 * num2;
+        return result;
+    }
+
+    public static int divide(int num1, int num2){
+        result = num1 / num2;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+        System.out.println(substract(5, 3));
+        System.out.println(multiply(5, 5));
+        System.out.println(divide(6, 3));
+    }
+}
