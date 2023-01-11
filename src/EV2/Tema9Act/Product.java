16d751092ba79212aba7e5d53413d013e0a79a9d
Ximo Llacer <xllaber7@gmail.com>
2023-01-11 09:34:26 +0100

    Create product get, set, const, toString

--- src/EV2/Tema9Act/Product.java
+++ src/EV2/Tema9Act/Product.java
@@ -1,5 +1,34 @@
 package EV2.Tema9Act;
 
 public class Product {
+    private int id;
+    private float price;
     
+    public Product(int id, float price) {
+        this.id = id;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString(){
+        return "{ID: " + this.id +
+                "\n Price: " + this.price + 
+                "\n}"; 
+    }
 }
